2977829b8f8b1891ba99c2dda3315a5a2d9d5553
Praveen Bandarage <bandarprav@ecs.vuw.ac.nz>
2020-12-07 14:04:40 +1300

    I implmented a delegate relationship between the Quote Cells and QuoteView controller such that I could implement the copy and share button.

--- Quotables/Controller/QuoteTableViewCell.swift
+++ Quotables/Controller/QuoteTableViewCell.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
-import SkeletonView
-class QuoteTVC: UITableViewCell {
-
+protocol QuoteTableViewCellDelegate: AnyObject {
+    func didTapShareButton(with quoteText: String, with authorName: String)
+    func didTapCopyButton(with quoteText: String, with authorName: String)
+    func didTapFavouriteButton(with quoteText: String, with authorText: String)
+}
+class QuoteTableViewCell: UITableViewCell {
+    
+    weak var delegate: QuoteTableViewCellDelegate?
     let buttonColour = UIColor(red: 0.19, green: 0.20, blue: 0.42, alpha: 1.00)
     
     @IBOutlet weak var authorLabel: UILabel!
@@ -18,11 +23,20 @@ class QuoteTVC: UITableViewCell {
     @IBOutlet weak var favouriteButton: UIButton!
     @IBOutlet weak var quoteView: UIView!
     override func awakeFromNib() {
-        
         super.awakeFromNib()
         setupMethod()
-        }
-
+    }
+    
+    
+    @IBAction func shareButtonPressed(_ sender: UIButton) {
+        delegate?.didTapShareButton(with: quoteLabel.text!, with: authorLabel.text!)
+    }
+    
+    
+    @IBAction func copyButtonPressed(_ sender: UIButton) {
+        delegate?.didTapCopyButton(with: quoteLabel.text!, with: authorLabel.text!)
+    }
+    
     
     //primarily created so that my awakeFromNIB function was tidier
     func setupMethod () {
@@ -31,11 +45,6 @@ class QuoteTVC: UITableViewCell {
         copyButton.layer.cornerRadius = 8
         copyButton.layer.borderWidth = 1
         copyButton.layer.backgroundColor = buttonColour.cgColor
-        
-//        shareButton.backgroundColor = .clear
-//        shareButton.layer.cornerRadius = 8
-//        shareButton.layer.borderWidth = 1
-//        shareButton.layer.backgroundColor = UIColor.black.cgColor
         
         favouriteButton.backgroundColor = .clear
         favouriteButton.layer.cornerRadius = 8
@@ -47,10 +56,7 @@ class QuoteTVC: UITableViewCell {
         shareButton.layer.borderWidth = 1
         shareButton.layer.backgroundColor = buttonColour.cgColor
     }
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-            
-        // Configure the view for the selected state
-    }
-
+    
+    
+    
 }
--- Quotables/Controller/QuoteTableViewController.swift
+++ Quotables/Controller/QuoteTableViewController.swift
@@ -10,54 +10,46 @@ import SkeletonView
 
 class QuoteTableViewController : UIViewController{
     
-
-    @IBOutlet weak var authorLabel: UILabel!
+    
     @IBOutlet weak var quoteTableView: UITableView!
     
     var quoteList = [Quote]()
     let quoteManager = QuoteManger()
     let gradient = SkeletonGradient(baseColor: UIColor.wisteria)
-
     
-    @IBOutlet weak var quoteLabel: UILabel!
     
-    @IBOutlet weak var copyButtonPressed: UIButton!
+    //
     
-    @IBAction func copyButtonPressed(_ sender: UIButton) {
-        
-        print(authorLabel.text!)
-        print(quoteLabel.text!)
-    }
     
     
     override func viewDidLoad() {
         self.navigationItem.setHidesBackButton(true, animated: true)
         self.navigationItem.title = "Home"
-
+        
         quoteTableView.isSkeletonable = true
         quoteManager.getQuotes()
         quoteTableView.showGradientSkeleton(usingGradient: gradient)
         quoteTableView.showSkeleton(transition: .crossDissolve(3))     //Show skeleton cross dissolve transition with 0.25 seconds fade time
-
+        
         while(quoteList.count == 0){
             quoteList = quoteManager.quoteList
-
+            
         }
         quoteTableView.isSkeletonable = false
         quoteTableView.stopSkeletonAnimation()
         quoteTableView.hideSkeleton(transition: .crossDissolve(3.25))     //Hide skeleton cross dissolve transition with 0.25 seconds fade time
-
+        
         quoteList.shuffle()
         super.viewDidLoad()
-
-
-
+        
+        
+        
         quoteTableView.dataSource = self
         //design adjustments
         quoteTableView.separatorStyle = .none
         quoteTableView.showsVerticalScrollIndicator = false
         quoteTableView.delegate = self
-//        quoteTableView.rowHeight = 250
+        //        quoteTableView.rowHeight = 250
         
     }
 }
@@ -73,7 +65,8 @@ extension QuoteTableViewController : UITableViewDataSource, UITableViewDelegate 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = quoteTableView.dequeueReusableCell(withIdentifier: "quoteCell") as! QuoteTVC
+        print("BUTTON PRESSED \(indexPath)")
+        let cell = quoteTableView.dequeueReusableCell(withIdentifier: "quoteCell") as! QuoteTableViewCell
         
         let quoteText = quoteList[indexPath.row].quote
         cell.quoteLabel.text = quoteText
@@ -82,13 +75,35 @@ extension QuoteTableViewController : UITableViewDataSource, UITableViewDelegate 
         //giving cell rounded view
         cell.quoteView.layer.cornerRadius = cell.quoteView.frame.height / 8
         cell.selectionStyle = UITableViewCell.SelectionStyle.none
-//        cell.isSkeletonable = true
-//        cell.showAnimatedGradientSkeleton()
-
+        //        cell.isSkeletonable = true
+        //        cell.showAnimatedGradientSkeleton()
+        cell.delegate = self
         return cell
     }
     
     
+}
+
+extension QuoteTableViewController: QuoteTableViewCellDelegate{
+    func didTapShareButton(with quoteText: String, with authorText: String) {
+        print(quoteText)
+        print(authorText)
+        
+        let stringToShare = "\(quoteText) - \(authorText)"
+        let activityViewController = UIActivityViewController(activityItems: [stringToShare], applicationActivities: nil)
+        
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func didTapCopyButton (with quoteText: String, with authorText: String) {
+        UIPasteboard.general.string = "\(quoteText) - \(authorText)"
+        
+    }
+    
+    func didTapFavouriteButton(with quoteText: String, with authorText: String) {
+        
+    }
     
     
 }
